2b92c7d0c91c0b450264b3f0af3d9babae5ce997
远方夕阳 <3979434@qq.com>
2019-07-04 03:42:52 +0800

    常规功能,UI优化，bug修复，以及文档更新

--- release/startup.bat
+++ release/startup.bat
@@ -1,1 +1,32 @@
-java -Dcom.sun.akuma.Daemon=daemonized -Dspring.profiles.active=pro  -jar lvxin-boot-server-3.0.0.jar  --server.host=192.168.1.1 --spring.datasource.url=jdbc:mysql://127.0.0.1:3306/lvxin_db --spring.datasource.username=root --spring.datasource.password=root --spring.redis.host=127.0.0.1
+﻿
+:: 注意等号前后不能有空格!!!!!!!
+:: 注意等号前后不能有空格!!!!!!!
+:: 注意等号前后不能有空格!!!!!!!
+
+
+::当前服务器ip
+set server_host=127.0.0.1
+
+::日志文件地址
+set log_file="C:\\logs\lvxin-server.log"
+
+::数据库连接 注意这个参数值必须要用引号
+set jdbc_url="jdbc:mysql://127.0.0.1:3306/lvxin_db?useUnicode=true&characterEncoding=utf8&serverTimezone=UTC"
+
+::数据库账号
+set jdbc_username=root
+
+::数据库密码
+set jdbc_password=root
+
+::redis服务器地址
+set redis_host=127.0.0.1
+
+::redis端口
+set redis_port=6379
+
+::redis密码，没有可以不设置,如果有密码请在命令最后面加上 --spring.redis.password=%redis_password% （注意前后需要一个空格）
+::set redis_password=
+
+
+java -Dcom.sun.akuma.Daemon=daemonized -Dspring.profiles.active=dev -jar ./lvxin-boot-server-3.0.0.jar --server.host=%server_host% --logging.file=%log_file% --spring.datasource.url=%jdbc_url% --spring.datasource.username=%jdbc_username% --spring.datasource.password=%jdbc_password% --spring.redis.host=%redis_host% 
--- release/startup.sh
+++ release/startup.sh
@@ -1,1 +1,33 @@
-java -Dcom.sun.akuma.Daemon=daemonized -Dspring.profiles.active=pro  -jar ./lvxin-boot-server-3.0.0.jar  --server.host=192.168.1.1 --spring.datasource.url=jdbc:mysql://127.0.0.1:3306/lvxin_db --spring.datasource.username=root --spring.datasource.password=root --spring.redis.host=127.0.0.1 &
+#! /bin/bash  
+
+# 注意等号前后不能有空格!!!!!!!
+# 注意等号前后不能有空格!!!!!!!
+# 注意等号前后不能有空格!!!!!!!
+
+
+#当前服务器ip
+server_host=127.0.0.1
+
+#日志文件地址
+log_file="/users/shared/logs/lvxin-server.log"
+
+#数据库连接 注意这个参数值必须要用引号
+jdbc_url="jdbc:mysql://127.0.0.1.com:3306/lvxin_db?useUnicode=true&characterEncoding=utf8&serverTimezone=UTC"
+
+#数据库账号
+jdbc_username=root
+
+#数据库密码
+jdbc_password=root
+
+#redis服务器地址
+redis_host=127.0.0.1
+
+#redis端口
+redis_port=6379
+
+#redis密码，没有可以不设置,如果有密码请在命令最后面 & 之前加上 --spring.redis.password=${redis_password} （注意前后需要一个空格）
+#redis_password=
+
+
+java -Dcom.sun.akuma.Daemon=daemonized -Dspring.profiles.active=dev -jar ./lvxin-boot-server-3.0.0.jar --server.host=${server_host} --logging.file=${log_file} --spring.datasource.url=${jdbc_url} --spring.datasource.username=${jdbc_username} --spring.datasource.password=${jdbc_password} --spring.redis.host=${redis_host} &
